04138cc0de5fce773adf4ee35db6d83ac36be58d
WendelSantosFS <isantos02061997@gmail.com>
2025-03-06 12:52:26 -0400

    final project

--- ContaBanco/src/BancoTerminal.java
+++ ContaBanco/src/BancoTerminal.java
@@ -1,9 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Locale;
+import java.util.Scanner;
+
 public class BancoTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       String nomeClient;
+       String agencia;
+       int numeroConta;
+       double saldo;
+
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+       System.out.println("Digite o número da Conta: ");
+       numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da Agência: ");
+        agencia = scanner.next();
+
+        System.out.println("Digite o NOME do cliente: ");
+        nomeClient = scanner.next();
+
+        System.out.println("Digite o Saldo do cliente: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println(
+        "Olá " + nomeClient + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta +
+                " e seu saldo R$" + saldo + " já está disponível para saque."
+        );
+
+
     }
 }
